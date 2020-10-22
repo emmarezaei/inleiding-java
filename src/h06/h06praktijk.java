db58d3500ac337f6c5819e6e00604d710e0ba750
emmarezaei <2134910@talnet.nl>
2020-10-22 10:46:16 +0200

    Hoofdstuk 6 Praktijk opdracht afgemaakt

--- src/h06/h06praktijk.java
+++ src/h06/h06praktijk.java
@@ -3,16 +3,18 @@ package h06;
 import java.awt.*;
 import java.applet.*;
 
-public class h06p3 extends Applet {
+public class h06praktijk extends Applet {
     double a, b, c, uitkomst;
+
+    int tijdelijkeUitkomst;
 
 
     public void init() {
         a = 5.9;
         b = 6.3;
         c = 6.9;
-        uitkomst = (a + b + c) / 3;
-
+        tijdelijkeUitkomst = (int) ((a + b + c) / 3 * 100);
+        uitkomst = (double) tijdelijkeUitkomst / 100;
     }
 
     public void paint(Graphics g) {
