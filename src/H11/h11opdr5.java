752c34a98b75090b770aaefefd2d2fc10501b8cf
emmarezaei <2134910@talnet.nl>
2020-11-17 10:28:44 +0100

    opdracht 5 van hoofdstuk 11 af

---
+++ src/H11/h11opdr5.java
@@ -0,0 +1,23 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class h11opdr5 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        int teller;
+        int x = 0;
+        int y = 0;
+
+        for (teller = 0; teller < 5; teller++) {
+//            g.drawString("" + teller, x, 20);
+            x += 20;
+            y += 20;
+            g.drawRect(x, y, 20, 20);
+        }
+    }
+}
